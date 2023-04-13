34e88be41687d6b747b7e58b8924c65dc3f688a1
Ryan B <bertramdryan@gmail.com>
2023-04-12 21:53:23 -0400

    first commit

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  # Association tests
+  it { should belong_to(:category) }
+
+  # validation
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:author)}
+  it { should validate_length_of(:title).is_at_least(3)}
+end
